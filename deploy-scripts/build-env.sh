bf816f8fe927bdd84f4cac19884e058d4704eff6
George Lambert <george@lambertlabs.com>
2019-04-27 16:28:08 +0100

    install zip

--- deploy-scripts/build-env.sh
+++ deploy-scripts/build-env.sh
@@ -9,6 +9,7 @@ conda update conda
 
 echo building environment
 conda env update --name root -f environment.yml
+apt-get install zip
 
 # Install linters globally
 apt-get install shellcheck
--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -5,6 +5,8 @@ set -xe
 echo "about to try and deploy"
 
 ls
+cd src
+ls
 zip target.zip -- *.py
 ls
 aws lambda update-function-code --function-name "$1" --zip-file fileb://target.zip
