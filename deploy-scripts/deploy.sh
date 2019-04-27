4d7932befeadf0173aca8ff846c98ca4d76f7fe2
George Lambert <george@lambertlabs.com>
2019-04-27 16:12:11 +0100

    updated

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -5,7 +5,7 @@ set -xe
 echo "about to try and deploy"
 
 ls
-zip target.zip *.py
+zip target.zip -- *.py
 ls
 aws lambda update-function-code --function-name "$1" --zip-file fileb://target.zip
 aws lambda publish-version --function-name "$1"
